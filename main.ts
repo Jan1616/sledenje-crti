b7b66592a5c8a38cf2c76cc976ce2c0976f89cfb
Jan1616 <60182539+Jan1616@users.noreply.github.com>
2020-03-03 16:55:45 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let hitrost_obracanja = 0
+let napaka = 0
+let obcutljivost = 1
+forever(function () {
+    motors.largeBC.setInverted(true)
+    napaka = 50 - sensors.color2.light(LightIntensityMode.Reflected)
+    hitrost_obracanja = obcutljivost * napaka
+    motors.largeBC.steer(hitrost_obracanja, 20)
+    brick.showNumber(sensors.color1.light(LightIntensityMode.Reflected), 1)
+    brick.showNumber(sensors.color2.light(LightIntensityMode.Reflected), 2)
+    brick.showNumber(hitrost_obracanja, 3)
+})
