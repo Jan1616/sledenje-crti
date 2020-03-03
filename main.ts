5e8749bb5a11be0e3a550ee33e10c48583c00bee
Jan1616 <60182539+Jan1616@users.noreply.github.com>
2020-03-03 16:56:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let hitrost_obracanja = 0
 let napaka = 0
-let obcutljivost = 1
+let obcutljivost = 2.2
 forever(function () {
     motors.largeBC.setInverted(true)
     napaka = 50 - sensors.color2.light(LightIntensityMode.Reflected)
     hitrost_obracanja = obcutljivost * napaka
-    motors.largeBC.steer(hitrost_obracanja, 20)
+    motors.largeBC.steer(hitrost_obracanja, 15)
     brick.showNumber(sensors.color1.light(LightIntensityMode.Reflected), 1)
     brick.showNumber(sensors.color2.light(LightIntensityMode.Reflected), 2)
     brick.showNumber(hitrost_obracanja, 3)
