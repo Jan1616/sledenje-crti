bbf06e0c87c6797c86537fca5a6118497d4230c8
Jan1616 <60182539+Jan1616@users.noreply.github.com>
2020-03-05 08:42:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
-let hitrost_obracanja = 0
-let napaka = 0
-let obcutljivost = 2.2
-forever(function () {
-    motors.largeBC.setInverted(true)
-    napaka = 50 - sensors.color2.light(LightIntensityMode.Reflected)
-    hitrost_obracanja = obcutljivost * napaka
-    motors.largeBC.steer(hitrost_obracanja, 15)
-    brick.showNumber(sensors.color1.light(LightIntensityMode.Reflected), 1)
-    brick.showNumber(sensors.color2.light(LightIntensityMode.Reflected), 2)
-    brick.showNumber(hitrost_obracanja, 3)
+function sledenje(cas: number, obcutljivost: number) {
+    let hitrost_obracanja = 0
+    let napaka = 0
+    control.timer1.reset()
+    while (control.timer1.seconds() < cas) {
+        motors.largeBC.setInverted(true)
+        napaka = 50 - sensors.color1.light(LightIntensityMode.Reflected)
+        hitrost_obracanja = obcutljivost * napaka
+        motors.largeBC.steer(hitrost_obracanja, 15)
+        brick.showNumber(sensors.color1.light(LightIntensityMode.Reflected), 1)
+        brick.showNumber(sensors.color2.light(LightIntensityMode.Reflected), 2)
+        brick.showNumber(hitrost_obracanja, 3)
+    }
+
+}
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    sledenje(5, 1.6)
+    motors.stopAll()
+    control.panic(5)
 })
+
+
